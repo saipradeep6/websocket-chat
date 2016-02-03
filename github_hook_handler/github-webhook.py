e5996b9f2034ef95d057895989ab9f0b49c5365a
saipradeep6 <saipradeep6@gmail.com>
2016-02-03 16:40:39 +0530

    Updated github-webhook.py file

--- github_hook_handler/github-webhook.py
+++ github_hook_handler/github-webhook.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
-import hashlib
+from flask import Flask, request, jsonify
 import subprocess
-#! import os
+import os
 
 app = Flask(__name__)
 
 @app.route("/payload", methods=['POST'])
 def github_payload():
     signature = request.headers.get('X-Hub-Signature')
-    data = request.data
-    if request.headers.get('X-GitHub-Event') == "ping":
-        return jsonify({'msg': 'Ok'})
-    if request.headers.get('X-GitHub-Event') == "push":
+    data = request.data    
+    if request.headers.get('X-GitHub-Event') == "push":  
         payload = request.get_json()
         if payload['commits'][0]['distinct'] == True:
             try:
-                cmd_output = subprocess.check_output(
-                    ['git', 'pull', 'origin', 'master'],)                    
-                return jsonify({'msg': str(cmd_output)})
-            except subprocess.CalledProcessError as error:                    
+                return jsonify({'msg': 'Push Event Successful'})
+            except subprocess.CalledProcessError as error:
                 return jsonify({'msg': str(error.output)})
+
         else:
             return jsonify({'msg': 'nothing to commit'})
 
+    else:
+        return jsonify({'msg': 'invalid event'})
+                
 if __name__ == "__main__":
     app.debug = True
     app.run(host="127.0.0.1")
