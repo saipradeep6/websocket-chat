c8830212fcc54866e1a4bf61813a5fa2a03b0959
saipradeep6 <saipradeep6@gmail.com>
2016-12-22 19:18:46 +0530

    Updated test.sh
    Rectified error with grep command

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-if [ git log --stat -1 | grep 'README' ] -o [ git log --stat -1 | grep 'index.scala.html' ]; then
+#!/bin/sh
+
+if [ git log --stat -1 | grep 'README' -o git log --stat -1 | grep 'index.scala.html' ]; then
     play test
 else
     exit
