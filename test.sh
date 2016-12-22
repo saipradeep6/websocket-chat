61e34e918891901c495b880e050c189007cbd666
saipradeep6 <saipradeep6@gmail.com>
2016-12-22 18:46:39 +0530

    Updated test.sh
    Changed the script

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-if git log --stat  -1 | grep 'README'; then
+if [git log --stat  -1 | grep 'README'] -o [git log --stat  -1 | grep 'index.scala.html']; then
     play test
 else
-    exit 3
+    exit
 fi
